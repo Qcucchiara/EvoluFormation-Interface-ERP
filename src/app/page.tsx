27c3ed6594f53cab4f0a7cc14ed4d9b6aa604bce
Phost <cucchiaraquentin@gmail.com>
2024-09-09 13:20:53 +0200

    forth try deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-// import { CreateNewProspect } from "@/components/Composites/CreateNewProspect";
-// import { LayoutDashboard } from "@/components/Layouts/LayoutDashboard";
-// import { FormModule } from "@/components/Modules/Forms/FormModule";
-// import { FormTrainer } from "@/components/Modules/Forms/FormTrainer";
-// import { FormDevis } from "@/components/Modules/Forms/Quote/FormDevis";
-// import { FormQuote } from "@/components/Modules/Forms/Quote/FormQuote";
-// import { Dashboard } from "../components/Others/V0/dashboard";
-// import { DashboardV2 } from "../components/Others/V0/dashboard-v2";
-// import DatatableExample from "../components/Others/V0/DatatableExample";
-// import GestionProspects from "../components/Others/V0/GestionProspects";
-// import { MessagingPage } from "../components/Others/V0/MessagingPage";
-// import Image from "next/image";
-
 // export default function Home() {
 //   return (
 //     <>
@@ -26,3 +13,10 @@
 //     </>
 //   );
 // }
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
