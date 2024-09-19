4f9dbb979c8f4009a379b43dde686c54f1b99a73
Phost <cucchiaraquentin@gmail.com>
2024-09-19 14:32:51 +0200

    add new page for company

--- src/app/entreprises/entreprise-detail/[companyId]/page.tsx
+++ src/app/entreprises/entreprise-detail/[companyId]/page.tsx
@@ -1,16 +1,52 @@
+import { FormStudent } from "@/components/Modules/Forms/FormStudent";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
 import React from "react";
 
 const page = ({ params }: { params: { companyId: string } }) => {
   return (
     <div>
       <div>{params.companyId.replace("'", "&apos;")}</div>
-      <ul>
+      <Tabs defaultValue="details" className="w-full">
+        <TabsList className="w-full">
+          <TabsTrigger value="details">Détails</TabsTrigger>
+          <TabsTrigger value="student">Stagiaires</TabsTrigger>
+        </TabsList>
+        <TabsContent value="details">
+          <Tabs defaultValue="infos" className="w-full">
+            <TabsList>
+              <TabsTrigger value="infos">Infos</TabsTrigger>
+              <TabsTrigger value="folders">Dossiers</TabsTrigger>
+            </TabsList>
+            <TabsContent value="infos"></TabsContent>
+            <TabsContent value="folders">
+              Liste des dossiers lié a une entreprise || overview du dossier de
+              l'entreprise
+            </TabsContent>
+          </Tabs>
+        </TabsContent>
+        <TabsContent value="student">
+          <Tabs defaultValue="list">
+            <TabsList>
+              <TabsTrigger value="list">Liste</TabsTrigger>
+              <TabsTrigger value="new">Nouveau Stagiaire</TabsTrigger>
+            </TabsList>
+            <TabsContent value="list"></TabsContent>
+            <TabsContent value="new">
+              <FormStudent />
+            </TabsContent>
+          </Tabs>
+        </TabsContent>
+      </Tabs>
+
+      {/* <ul>
+        <li>détail entreprise/prospect lié/ etc...</li>
         <li>liste des élèves</li>
         <li>formulaire nouvel élève</li>
-        <li>détail entreprise/prospect lié/ etc...</li>
         <li>formulaire update entreprise</li>
         <li>je sais pas quoi d&apos;autre encore</li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
